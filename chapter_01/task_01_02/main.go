1a20fb8c2043dc4a16e1bb6c1202237301a069e1
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-11-01 13:06:41 +0300

    Упражнение 1.2: добавил решение к заданию

--- chapter_01/task_01_02/main.go
+++ chapter_01/task_01_02/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 // Упражнение 1.2.
 //
 // Измените программу echo так, чтобы она выводила индекс и значение каждого аргумента по одному аргументу в строке.
 func main() {
-	fmt.Println(strings.Join(os.Args[:], " "))
+	for inx, value := range os.Args[:] {
+		fmt.Println(inx, "-", value)
+	}
 }
