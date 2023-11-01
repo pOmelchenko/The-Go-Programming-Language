73e5385cb8483288ea6be32d270b6c9a38735e4a
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-11-01 13:03:09 +0300

    Упражнение 1.1: добавил текст задания

--- chapter_01/task_01_01/main.go
+++ chapter_01/task_01_01/main.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 )
 
+// Упражнение 1.1.
+//
+// Измените программу echo так, чтобы она выводила также os.Args[0], имя выполняемой команды.
 func main() {
 	fmt.Println(strings.Join(os.Args[:], " "))
 }
